ef205455d986dc9f300a04d2d7eb3f987398b185
Kohei Yoshino <kohei.yoshino@gmail.com>
2020-07-22 00:40:04 -0400

    画像の拡大表示を実装
    Fix https://github.com/webdino/stage.webdino.org/issues/92

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,5 +1,15 @@
+/* eslint-disable indent */
+/* eslint-disable no-var */
+/* eslint-disable object-shorthand */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable prefer-template */
+/* eslint-disable require-jsdoc */
+/* eslint-disable semi */
+
 // Load fonts from Typekit
-try { Typekit.load({ async: true }); } catch (ex) {}
+try { Typekit.load({ async: true }); } catch (ex) { }
 
 // Load Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -11,19 +21,19 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-6459738-14', 'auto');
 ga('send', 'pageview');
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
   // Track outbound links
-  [].forEach.call(document.querySelectorAll('a[href^="http"]'), function($link) {
-    $link.addEventListener('click', function(event) {
+  [].forEach.call(document.querySelectorAll('a[href^="http"]'), function ($link) {
+    $link.addEventListener('click', function (event) {
       ga('send', 'event', 'Outbound Link', 'click', $link.href, {
-        'transport': 'beacon',
-        'hitCallback': function() { document.location = $link.href; }
+        transport: 'beacon',
+        hitCallback: function () { document.location = $link.href; }
       });
 
       // Fallback when the `hitCallback` function doesn't work for some reason
-      window.setTimeout(function() { document.location = $link.href; }, 1000);
+      window.setTimeout(function () { document.location = $link.href; }, 1000);
 
       event.preventDefault();
       event.stopPropagation();
@@ -32,22 +42,24 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  // Activate mobile navigation UI
+  /**
+   * Activate mobile navigation UI.
+   */
   function activate_drawer() {
     var $body = document.body;
     var $drawer = document.querySelector('#drawer');
 
-    document.querySelector('#header .mobile[role="button"]').addEventListener('mousedown', function(event) {
+    document.querySelector('#header .mobile[role="button"]').addEventListener('mousedown', function (event) {
       $body.classList.add('menu-expanded');
       $drawer.setAttribute('aria-expanded', 'true');
     });
 
-    $drawer.querySelector('.mobile[role="button"]').addEventListener('mousedown', function(event) {
+    $drawer.querySelector('.mobile[role="button"]').addEventListener('mousedown', function (event) {
       $body.classList.remove('menu-expanded');
       $drawer.setAttribute('aria-expanded', 'false');
     });
 
-    $drawer.addEventListener('mousedown', function(event) {
+    $drawer.addEventListener('mousedown', function (event) {
       if ($drawer.getAttribute('aria-expanded') === 'true') {
         $body.classList.remove('menu-expanded');
         $drawer.setAttribute('aria-expanded', 'false');
@@ -55,13 +67,15 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     });
 
-    $drawer.querySelector('.inner').addEventListener('mousedown', function(event) {
+    $drawer.querySelector('.inner').addEventListener('mousedown', function (event) {
       event.stopPropagation();
     });
   }
 
-  // Activate Ajax form submission
-  // https://www.netlify.com/docs/form-handling/#ajax-form-submissions
+  /**
+   * Activate Ajax form submission
+   * @see https://www.netlify.com/docs/form-handling/#ajax-form-submissions
+   */
   function activate_contact_form() {
     var $form = document.querySelector('form[name="contact"]');
 
@@ -69,7 +83,7 @@ window.addEventListener('DOMContentLoaded', function() {
       return;
     }
 
-    $form.addEventListener('submit', function(event) {
+    $form.addEventListener('submit', function (event) {
       event.preventDefault();
 
       var data = [];
@@ -87,19 +101,81 @@ window.addEventListener('DOMContentLoaded', function() {
           continue;
         }
 
-        data.push([encodeURIComponent(elements[i].name), encodeURIComponent(elements[i].value)].join('='))
+        data.push([encodeURIComponent(elements[i].name), encodeURIComponent(elements[i].value)].join('='));
       }
 
-      xhr.addEventListener('load', function() { $thanks.hidden = false; $thanks.focus(); });
-      xhr.addEventListener('error', function() { $error.hidden = false; $error.focus(); });
-      xhr.addEventListener('abort', function() { $error.hidden = false; $error.focus(); });
-      xhr.addEventListener('loadend', function() { $main.hidden = $submit.hidden = true; });
+      xhr.addEventListener('load', function () { $thanks.hidden = false; $thanks.focus(); });
+      xhr.addEventListener('error', function () { $error.hidden = false; $error.focus(); });
+      xhr.addEventListener('abort', function () { $error.hidden = false; $error.focus(); });
+      xhr.addEventListener('loadend', function () { $main.hidden = $submit.hidden = true; });
       xhr.open('POST', $form.action);
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       xhr.send(data.join('&'));
-    })
+    });
+  }
+
+  /**
+   * Activate the image zoom functionality on blog posts.
+   */
+  function activate_image_zoom() {
+    // Skip IE support
+    if (typeof NodeList.prototype.forEach !== 'function' || typeof Object.assign !== 'function') {
+      return;
+    }
+
+    document.querySelectorAll('article.post figure img').forEach(function ($img) {
+      $img.classList.add('can-zoom');
+
+      $img.addEventListener('click', function () {
+        const rect = $img.getBoundingClientRect();
+        const $base = document.createElement('div');
+        const $container = $base.appendChild(document.createElement('div'));
+        const $_img = $container.appendChild($img.cloneNode(true));
+
+        $base.classList.add('image-zoom-base');
+
+        Object.assign($container.style, {
+          top: rect.top + 'px',
+          left: rect.left + 'px',
+          width: rect.width + 'px',
+          height: rect.height + 'px',
+        });
+
+        function onclick() {
+          close();
+        }
+
+        function onkeydown(event) {
+          if (event.key === 'Escape') {
+            close();
+          }
+        }
+
+        function close() {
+          document.removeEventListener('keydown', onkeydown);
+          $base.removeEventListener('click', onclick);
+          $base.classList.remove('active');
+
+          window.setTimeout(function () {
+            document.body.removeChild($base);
+            document.body.classList.remove('overlay-visible');
+          }, 600);
+        }
+
+        document.addEventListener('keydown', onkeydown);
+        $base.addEventListener('click', onclick);
+
+        document.body.appendChild($base);
+        document.body.classList.add('overlay-visible');
+
+        window.setTimeout(function () {
+          $base.classList.add('active');
+        }, 100);
+      });
+    });
   }
 
   activate_drawer();
   activate_contact_form();
+  activate_image_zoom();
 }, { once: true });
