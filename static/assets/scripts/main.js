60300ddf616a657a117546e4fb2b1db834d4d1b6
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-05-30 18:54:37 -0400

    Google Analytics 4 へ移行

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,43 +1,5 @@
-/* eslint-disable indent */
-/* eslint-disable no-var */
-/* eslint-disable object-shorthand */
-/* eslint-disable prefer-arrow-callback */
-/* eslint-disable prefer-destructuring */
-/* eslint-disable prefer-rest-params */
-/* eslint-disable prefer-template */
-/* eslint-disable require-jsdoc */
-/* eslint-disable semi */
-
-// Load Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-// Track page view
-ga('create', 'UA-6459738-14', 'auto');
-ga('send', 'pageview');
-
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
-
-  // Track outbound links
-  [].forEach.call(document.querySelectorAll('a[href^="http"]'), function ($link) {
-    $link.addEventListener('click', function (event) {
-      ga('send', 'event', 'Outbound Link', 'click', $link.href, {
-        transport: 'beacon',
-        hitCallback: function () { document.location = $link.href; }
-      });
-
-      // Fallback when the `hitCallback` function doesn't work for some reason
-      window.setTimeout(function () { document.location = $link.href; }, 1000);
-
-      event.preventDefault();
-      event.stopPropagation();
-
-      return false;
-    });
-  });
 
   /**
    * Activate mobile navigation UI.
